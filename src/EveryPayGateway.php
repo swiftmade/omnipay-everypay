acb28fa28bec0c8bcf54c0f5b731ac609290af07
Ahmet Özışık <ozisikahmet@gmail.com>
2019-09-03 15:11:08 +0300

    extend abstract gateway, add username & secret as params

--- src/EveryPayGateway.php
+++ src/EveryPayGateway.php
@@ -1,8 +1,45 @@
 <?php
 namespace Swiftmade\EveryPay;
 
-use Omnipay\Common\GatewayInterface;
+use Omnipay\Common\AbstractGateway;
 
-class EveryPayGateway implements GatewayInterface
+class EveryPayGateway extends AbstractGateway
 {
+    public function getName()
+    {
+        return 'Every Pay';
+    }
+
+    public function getShortName()
+    {
+        return 'everyPay';
+    }
+
+    public function getDefaultParameters()
+    {
+        return [
+            'username' => '',
+            'secret' => '',
+        ];
+    }
+
+    public function getUsername()
+    {
+        return $this->getParameter('username');
+    }
+
+    public function setUsername($username)
+    {
+        return $this->setParameter('username', $username);
+    }
+
+    public function getSecret()
+    {
+        return $this->getParameter('secret');
+    }
+
+    public function setSecret($secret)
+    {
+        return $this->setParameter('secret', $secret);
+    }
 }
