f3c9149935abe2650ebce4bcf571c073aea49afe
Ahmet Özışık <ozisikahmet@gmail.com>
2019-09-13 10:53:13 +0300

    refactor SignedData parameters into a new class

---
+++ src/Support/SignedDataOptions.php
@@ -0,0 +1,48 @@
+<?php
+namespace Omnipay\EveryPay\Support;
+
+class SignedDataOptions
+{
+    /**
+     * @array
+     */
+    protected $options;
+
+    public function __construct(array $options)
+    {
+        $this->options = $options;
+    }
+
+    public static function gateway($secret)
+    {
+        return new SignedDataOptions([
+            'secret' => $secret,
+            'appendHmacFields' => true,
+            'dontInclude' => ['locale'],
+        ]);
+    }
+
+    public static function backend($secret)
+    {
+        return new SignedDataOptions([
+            'secret' => $secret,
+            'appendHmacFields' => false,
+            'dontInclude' => ['locale'],
+        ]);
+    }
+
+    public function shouldHmacInclude($field)
+    {
+        return !in_array($field, $this->options['dontInclude']);
+    }
+
+    public function shouldAppendHmacFields()
+    {
+        return $this->options['appendHmacFields'];
+    }
+
+    public function secret()
+    {
+        return $this->options['secret'];
+    }
+}
