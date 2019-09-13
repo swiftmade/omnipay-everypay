73c19bd2468fafa8d63dd5ca2e1afbc36d1be52a
Ahmet Özışık <ozisikahmet@gmail.com>
2019-09-13 10:53:26 +0300

    create separate request and response for backend purchase handling

--- src/Messages/BackendPurchaseResponse.php
+++ src/Messages/BackendPurchaseResponse.php
@@ -25,15 +25,30 @@ class BackendPurchaseResponse extends AbstractResponse implements RedirectRespon
     {
         $response = @json_decode($this->data, true);
 
-        if (!is_array($response) || !isset($response['charge'])) {
-            $this->message = 'Error';
+        if (!is_array($response) || (!isset($response['errors'], $response['charge']))) {
+            $this->message = 'Unrecognized response format';
+            return false;
+        }
+
+        if (isset($response['errors'])) {
+            $this->message = $response['errors'][0]['message'];
             return false;
         }
 
         $charge = $response['charge'];
-        // TODO: implement
 
-        return false;
+        if (!in_array($charge['payment_state'], ['authorised', 'settled'])) {
+            $this->message = 'Payment has failed - ' . $charge['payment_state'];
+            return false;
+        }
+
+        if ($charge['order_reference'] !== $this->request->getTransactionId()) {
+            $this->message = 'Transaction ID mismatch.';
+            return false;
+        }
+
+        dd($charge);
+        return true;
     }
 
     public function isRedirect()
